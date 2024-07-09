a256d52886aa59761fc15fa4ebfcf64cd4edb53e
donicnu <donicnu@gmail.com>
2024-07-10 00:50:00 +0900

    fix: modify root inder render logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import './css/index.css';
-import theme from './app/MaterialTheme';
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import "./css/index.css";
+import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
 
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <App />
-      </Router>
-      
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
