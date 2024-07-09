69faa6de832ab777131a2f1b0541209e97facd5d
donicnu <donicnu@gmail.com>
2024-07-10 00:44:23 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 
@@ -11,6 +10,9 @@ import { HomePage } from "./screens/homePage";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { Footer } from "./components/footer";
+import "../css/app.css";
+import "../css/navbar.css";
+import { HelpPage } from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
@@ -28,6 +30,9 @@ function App() {
         </Route>
         <Route path="/member-page">
           <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
         </Route>
         <Route path="/">
           <HomePage />
