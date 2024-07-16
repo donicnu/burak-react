366697a8a7d853f2db0473b8a210bea5590a6c12
donicnu <donicnu@gmail.com>
2024-07-17 03:39:06 +0900

    feat: publish orderPage screen component

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -1,6 +1,118 @@
-import { Container } from "@mui/material";
+import { useState, SyntheticEvent } from "react";
+import { Container, Stack, Box } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PausedOrders from "./PausedOrders";
+import ProcessOrders from "./ProcessOrders";
+import FinishedOrders from "./FinishedOrders";
+import "../../../css/order.css";
 
 export default function OrdersPage() {
-  //camel case
-  return <Container>OrdersPage</Container>;
+  const [value, setValue] = useState("1"); //bydefault 1 ni ochib berishi yani paused ordersni
+
+  const handleChange = (e: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={"order-page"}>
+      <Container className={"order-container"}>
+        <Stack>
+          <TabContext value={value}>
+            <Box className={"order-nav-frame"}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  className={"table_list"}
+                >
+                  <Tab label="PAUSED ORDERS" value={"1"} />
+                  <Tab label="PROCESS ORDERS" value={"2"} />
+                  <Tab label="FINISHED ORDERS" value={"3"} />
+                </Tabs>
+              </Box>
+            </Box>
+            <Stack className={"order-main-content"}>
+              <PausedOrders />
+              <ProcessOrders />
+              <FinishedOrders />
+            </Stack>
+          </TabContext>
+        </Stack>
+
+        <Stack className="order-right">
+          <Stack className="order-info-box">
+            <Stack className="member-box">
+              <div className="order-user-img">
+                <img
+                  src={"icons/default-user.svg"}
+                  className="order-user-avatar"
+                />
+                <div className="order-user-icon-box">
+                  <img
+                    src={"icons/user-badge.svg"}
+                    className="order-user-prof-img"
+                  />
+                </div>
+              </div>
+              <span className="order-user-name">Danny</span>
+              <span className="order-user-prof">User</span>
+            </Stack>
+            <Box className="liner"></Box>
+            <Stack sx={{ flexDirection: "row", marginLeft: "-100px" }}>
+              <LocationOnIcon />
+              <Box className="spec-address-txt">Uzbekistan, Djizak</Box>
+            </Stack>
+          </Stack>
+          <Box className="order-info-box" sx={{ marginTop: "23px" }}>
+            <input
+              type="text"
+              className="card-input"
+              placeholder="Card number : **** 5678 9123 1234"
+            />
+            <Stack
+              sx={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <input
+                type="text"
+                className="card-half-input"
+                placeholder="07 / 24"
+              />
+              <input
+                type="text"
+                className="card-half-input"
+                placeholder="CVV : 010"
+              />
+            </Stack>
+            <input
+              type="text"
+              className="card-input"
+              placeholder="Justin Robertson"
+            />
+            <Stack className="cards-box">
+              <img
+                src={"/icons/master-card.svg"}
+                style={{ marginLeft: "20px" }}
+              />
+              <img
+                src={"/icons/paypal-card.svg"}
+                style={{ marginLeft: "20px" }}
+              />
+              <img
+                src={"/icons/visa-card.svg"}
+                style={{ marginLeft: "20px" }}
+              />
+              <img
+                src={"/icons/western-card.svg"}
+                style={{ marginLeft: "20px" }}
+              />
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
