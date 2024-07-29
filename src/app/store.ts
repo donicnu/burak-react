e2aa24499d168908468d37af1a5d9c66e7f1da36
donicnu <donicnu@gmail.com>
2024-07-30 03:46:17 +0900

    feat: middleware integration redux-logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,17 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/homePage/slice";
+import reduxLogger from "redux-logger";
+//reduxlogger redux ichida qaysi malumotlar ozgaryotganini bizga aniq qilib beradi
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    //@ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
   reducer: {
     homePage: HomePageReducer,
   },
