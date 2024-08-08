9a3925a89e67b18f2f8581e5152e383edbffe1b8
donicnu <donicnu@gmail.com>
2024-08-09 02:46:22 +0900

    feat: develop createOrder and retrieve paused orders data

--- src/app/screens/ordersPage/PausedOrders.tsx
+++ src/app/screens/ordersPage/PausedOrders.tsx
@@ -31,6 +31,7 @@ export default function PausedOrders() {
                     (ele: Product) => item.productId === ele._id
                   )[0];
                   const imagePath = `${serverApi}/${product.productImages[0]}`;
+
                   return (
                     <Box key={item._id} className="orders-name-price">
                       <img src={imagePath} className="order-dish-img" />
