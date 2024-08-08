44bb0311b1120f640cce4ed1f5d196b79b505cff
donicnu <donicnu@gmail.com>
2024-08-09 03:09:09 +0900

    fix: modify processOrders and finishedOrders components

--- src/app/screens/ordersPage/FinishedOrders.tsx
+++ src/app/screens/ordersPage/FinishedOrders.tsx
@@ -4,25 +4,45 @@ import Button from "@mui/material/Button";
 import TabPanel from "@mui/lab/TabPanel";
 import moment from "moment";
 
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveFinishedOrders } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { serverApi } from "../../../lib/config";
+import { Order, OrderInquiry, OrderItem } from "../../../lib/types/order";
+
+/** REDUX SLICE & SELECTOR */
+const finishedOrdersRetriever = createSelector(
+  retrieveFinishedOrders,
+  (finishedOrders) => ({ finishedOrders })
+);
+
 export default function FinishedOrders() {
+  const { finishedOrders } = useSelector(finishedOrdersRetriever);
   return (
     <TabPanel value={"3"}>
       <Stack>
-        {[].map((ele, index) => {
+        {finishedOrders?.map((order: Order) => {
           return (
-            <Box key={index} className="order-main-box">
+            <Box key={order._id} className="order-main-box">
               <Box className="order-box-scroll">
-                {[1, 2, 3].map((ele2, index2) => {
+                {order.orderItems?.map((item: OrderItem) => {
+                  const product: Product = order.productData.filter(
+                    (ele: Product) => item.productId === ele._id
+                  )[0];
+                  const imagePath = `${serverApi}/${product.productImages[0]}`;
                   return (
-                    <Box key={index2} className="orders-name-price">
+                    <Box key={item._id} className="orders-name-price">
                       <img src={"/img/kebab.webp"} className="order-dish-img" />
-                      <p className="title-dish">Kebab</p>
+                      <p className="title-dish">{product.productName}</p>
                       <Box className="price-box">
-                        <p>$11</p>
+                        <p>${item.itemPrice}</p>
                         <img src={"/icons/close.svg"} />
-                        <p>2</p>
+                        <p>{item.itemQuantity}</p>
                         <img src={"/icons/pause.svg"} />
-                        <p style={{ marginLeft: "15px" }}>$22</p>
+                        <p style={{ marginLeft: "15px" }}>
+                          ${item.itemQuantity * item.itemPrice}
+                        </p>
                       </Box>
                     </Box>
                   );
@@ -32,29 +52,35 @@ export default function FinishedOrders() {
               <Box className="total-price-box">
                 <Box className="box-total">
                   <p>Product price</p>
-                  <p>$22</p>
+                  <p>${order.orderTotal - order.orderDelivery}</p>
                   <img src={"/icons/plus.svg"} style={{ marginLeft: "20px" }} />
                   <p>Delivery cost</p>
-                  <p>$2</p>
+                  <p>${order.orderDelivery}</p>
                   <img
                     src={"/icons/pause.svg"}
                     style={{ marginLeft: "20px" }}
                   />
                   <p>Total</p>
-                  <p>$24</p>
+                  <p>${order.orderTotal}</p>
                 </Box>
               </Box>
             </Box>
           );
         })}
-        {true && (
-          <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
-            <img
-              src={"/icons/noimage-list.svg"}
-              style={{ width: 300, height: 300 }}
-            />
-          </Box>
-        )}
+
+        {!finishedOrders ||
+          (finishedOrders.length === 0 && (
+            <Box
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"center"}
+            >
+              <img
+                src={"/icons/noimage-list.svg"}
+                style={{ width: 300, height: 300 }}
+              />
+            </Box>
+          ))}
       </Stack>
     </TabPanel>
   );
